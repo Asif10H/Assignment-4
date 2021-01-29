5204726f6a4abd00bccfc88dcb14be7e2285b00e
Asif10H <habibullahasif71@gmail.com>
2021-01-30 01:48:03 +0600

    added function for first-class and economy

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-// plus button for First Class 
-document.getElementById("first-class-increase").addEventListener("click", function(){
-    const seatInput = document.getElementById("first-class-seat-count");
-    const seatCount = parseInt(seatInput.value);
-    const newSeatCount = seatCount + 1;
-    seatInput.value = newSeatCount;
-    document.getElementById("first-class-seat-count").innerText = newSeatCount;
-})
-// minus button for First Class
-document.getElementById("first-class-decrease").addEventListener("click", function(){
-    const seatInput = document.getElementById("first-class-seat-count");
-    const seatCount = parseInt(seatInput.value);
-    const newSeatCount = seatCount - 1;
-    seatInput.value = newSeatCount;
-    document.getElementById("first-class-seat-count").innerText = newSeatCount;
-})
+//seat change function
+function seatChange(seat , isIncrease){
+    const seatInput = document.getElementById(seat);
+    const seatCount = getUserInputValue(seat)
+    let newSeatCount = seatCount;
+    if(isIncrease == true){ // seat increase 
+        newSeatCount = seatCount + 1;
+    }
+    else if(isIncrease == false && seatCount > 0){ // seat decrease
+        newSeatCount = seatCount - 1;
+    }
+    seatInput.value =  newSeatCount;
+}
 
-// plus button for Economy 
-document.getElementById("economy-increase").addEventListener("click", function(){
-    const seatInput = document.getElementById("economy-seat-count");
+//input function
+function getUserInputValue(seat){
+    const seatInput = document.getElementById(seat);
     const seatCount = parseInt(seatInput.value);
-    const newSeatCount = seatCount + 1;
-    seatInput.value = newSeatCount;
-    document.getElementById("economy-seat-count").innerText = newSeatCount;
-})
-// minus button for Economy 
-document.getElementById("economy-decrease").addEventListener("click", function(){
-    const seatInput = document.getElementById("economy-seat-count");
-    const seatCount = parseInt(seatInput.value);
-    const newSeatCount = seatCount - 1;
-    seatInput.value = newSeatCount;
-    document.getElementById("economy-seat-count").innerText = newSeatCount;
-})
+    return seatCount;
+}
