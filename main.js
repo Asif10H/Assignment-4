621f835cf13951789d54b3df0dc5b6e1783fabac
Asif10H <habibullahasif71@gmail.com>
2021-01-30 14:02:20 +0600

    order section updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//seat change function
-function seatChange(seat , isIncrease){
+//seat count function
+function seatCount(seat , isIncrease){
     const seatInput = document.getElementById(seat);
     const seatCount = getUserInputValue(seat)
     let newSeatCount = seatCount;
@@ -25,21 +25,29 @@ function priceCalculate(){
     //Subtotal
     const subtotalPrice = firstClassSetCount * 150 + economySetCount * 100; // First Class seat price ($150) and Economy seat price ($100)
     document.getElementById("sub-total-price").innerText = "$" + subtotalPrice;
+    document.getElementById("order-sub-total-price").innerText = "$" + subtotalPrice;// subtotal for order section
     //Charge 10% VAT
     const totalVat = Math.round(subtotalPrice * 0.1);
     document.getElementById("vat").innerText = "$" + totalVat;
+    document.getElementById("order-vat").innerText = "$" + totalVat;// vat for order section
     //Total
     const totalPrice = subtotalPrice + totalVat;
     document.getElementById("total-price").innerText = "$" +  totalPrice;
-}
-// order received
-function orderDetails(){
-    //
+    document.getElementById("order-total-price").innerText = "$" +  totalPrice; // total price for order section
 }
 // hide section
 document.getElementById("booking-btn").addEventListener("click", function(){
     const sectionHide = document.getElementById("hide-section");
     sectionHide.style.display = "none";
-    const orderReceivedSection = document.getElementById("order-received");
-    orderReceivedSection.style.display = "block";
+    const firstClassSetCount = getUserInputValue("first-class-count");
+    const economySetCount = getUserInputValue("economy-count");
+    if(firstClassSetCount != 0 || economySetCount != 0){
+        const orderReceivedSection = document.getElementById("order-received");
+        orderReceivedSection.style.display = "block";
+    }
+    else{
+        alert("please at least one seat booking first.");
+        const sectionHide = document.getElementById("hide-section");
+        sectionHide.style.display = "block";
+    }
 })
